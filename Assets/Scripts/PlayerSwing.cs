e4280583d24ac4111dfdc4252a3a73f163e83005
Jeffrey Thor <thorjeffrey@gmail.com>
2018-12-29 19:15:40 -0500

    Alternates punching arms

--- Assets/Scripts/PlayerSwing.cs
+++ Assets/Scripts/PlayerSwing.cs
@@ -3,37 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSwing : MonoBehaviour {
+  private GameObject rightArm;
+  private GameObject leftArm;
+  private bool attacking;
+  private Vector3 startPosition;
+  private GameObject attackArm;
 
-	private GameObject rightArm;
-	private GameObject leftArm;
-	private bool attacking;
-	private Vector3 startPosition;
+  // Use this for initialization
+  void Start() {
+    attacking = false;
+    rightArm = transform.Find("RightArmPlayer").gameObject;
+    leftArm = transform.Find("LeftArmPlayer").gameObject;
+    attackArm = leftArm;
+    startPosition = attackArm.transform.localPosition;
+  }
 
-	// Use this for initialization
-	void Start () {
-		attacking = false;
-		rightArm = transform.Find("RightArmPlayer").gameObject;
-		leftArm = transform.Find("LeftArmPlayer").gameObject;
-		startPosition = rightArm.transform.localPosition;
-		Debug.Log("x: " + startPosition.x + " y: " + startPosition.y + " z: " + startPosition.z);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!attacking) {
-			if (Input.GetButton("Fire1")) {
-				Debug.Log("Attack!");
-				attacking = true;
-			}
-		}
+  // Update is called once per frame
+  void Update() {
+    if (!attacking) {
+      if (Input.GetButton("Fire1")) {
+        attacking = true;
+        if (attackArm == rightArm) {
+          attackArm = leftArm;
+        } else {
+          attackArm = rightArm;
+        }
+        startPosition = attackArm.transform.localPosition;
+      }
+    }
 
-		if (attacking) {
-			rightArm.transform.localPosition = new Vector3(rightArm.transform.localPosition.x, rightArm.transform.localPosition.y, rightArm.transform.localPosition.z + 0.05f);
-			if (rightArm.transform.localPosition.z >= startPosition.z + 0.5f) {
-				rightArm.transform.localPosition = startPosition;
-				Debug.Log("Done.");
-				attacking = false;
-			}
-		}
-	}
+    if (attacking) {
+      attackArm.transform.localPosition = new Vector3(attackArm.transform.localPosition.x,
+        attackArm.transform.localPosition.y, attackArm.transform.localPosition.z + (2 * Time.deltaTime));
+      if (attackArm.transform.localPosition.z >= startPosition.z + 0.5f) {
+        attackArm.transform.localPosition = startPosition;
+        attacking = false;
+      }
+    }
+  }
 }
