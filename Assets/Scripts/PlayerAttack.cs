1673e0e408bc90d31a25aefce025153e06a3d463
Jeffrey Thor <thorjeffrey@gmail.com>
2019-01-24 11:42:34 -0500

    Updates health bars based on damage received

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
-  private float damage = 10f;
-  private float range = 5f;
+  private Client client;
+  private int damage = 10;
+  private float range = 10f;
   private GameObject arms;
 
-  private void Update() {
-    Debug.DrawRay(gameObject.transform.position, transform.forward, Color.red);
+  private void Start() {
+    client = GameObject.Find("Client").GetComponent<Client>();
   }
 
   public void Attack() {
     RaycastHit hit;
-    if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, range)) {
+    if (Physics.Raycast(gameObject.transform.position, transform.forward, out hit, range)) {
+      client.SendReliable("HIT|" + hit.transform.gameObject.GetComponent<PlayerController>().id + "|" + damage);
       Debug.Log(hit.transform.name);
+      hit.transform.gameObject.GetComponent<PlayerController>().TakeDamage(damage);
     }
   }
 }
