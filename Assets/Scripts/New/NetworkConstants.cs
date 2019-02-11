1e6c36fcd54c2e01df0796b0e07c1d2ab52e9aae
Jeffrey Thor <thorjeffrey@gmail.com>
2019-02-11 11:29:59 -0500

    Removes network constants

--- Assets/Scripts/New/NetworkConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace New {
-  public static class NetworkConstants {
-    public const string Address = "34.207.71.203";
-    public const int Port = 7777;
-    public const string Api = "http://www.lerocia.com/api/";
-    public const int MaxConnection = 100;
-  }
-}
