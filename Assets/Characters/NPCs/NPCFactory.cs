cec95ec0921d272c736ea69974652db6bd394673
Jeffrey Thor <thorjeffrey@gmail.com>
2019-02-18 16:56:21 -0500

    Adds new dialogues and handles NPC position and rotation

--- Assets/Characters/NPCs/NPCFactory.cs
+++ Assets/Characters/NPCs/NPCFactory.cs
@@ -1,5 +1,4 @@
 namespace Characters.NPCs {
-  using System.Collections.Generic;
   using UnityEngine;
   using Animation;
   using Characters.Controllers;
@@ -12,42 +11,22 @@ namespace Characters.NPCs {
       // Spawn all NPCs
       for (int i = 1; i < data.Length; i++) {
         string[] d = data[i].Split('%');
-        Spawn(int.Parse(d[0]), d[1], float.Parse(d[2]), float.Parse(d[3]), float.Parse(d[4]));
+        Spawn(int.Parse(d[0]), d[1], float.Parse(d[2]), float.Parse(d[3]), float.Parse(d[4]), float.Parse(d[5]),
+          float.Parse(d[6]), float.Parse(d[7]));
       }
     }
 
-    public void Spawn(int npcId, string npcName, float x, float y, float z) {
+    public void Spawn(int npcId, string npcName, float px, float py, float pz, float rx, float ry, float rz) {
       GameObject npcObject = Instantiate(NPCPrefab);
       npcObject.name = npcName;
-      npcObject.transform.position = new Vector3(x, y, z);
+      npcObject.transform.position = new Vector3(px, py, pz);
+      npcObject.transform.rotation = Quaternion.Euler(new Vector3(rx, ry, rz));
       npcObject.AddComponent<NPCController>();
       npcObject.AddComponent<CharacterLerpController>();
       npcObject.AddComponent<NPCReference>();
       npcObject.GetComponent<NPCReference>().NPCId = npcId;
       npcObject.AddComponent<CharacterAnimator>();
-      Dictionary<string, Dialogue> dialogues = new Dictionary<string, Dialogue> {
-        {
-          "Talk",
-          new Dialogue("Hi! I'm a friendly NPC, want to talk?", new[] {"Yea, sure.", "Nope."})
-        },
-        {
-          "Yea, sure.",
-          new Dialogue("Cool! I don't have a whole lot to talk about but if you kill me you can take all my items!", new[] {"That's kind of messed up...", "Thanks for the tip."})
-        },
-        {
-          "Nope.",
-          new Dialogue("Ok, have a good one.", null)
-        },
-        {
-          "That's kind of messed up...",
-          new Dialogue("It is a little bit, but that's just how this game works. Happy killing!", null)
-        },
-        {
-          "Thanks for the tip.",
-          new Dialogue("No problem!", null)
-        }
-      };
-      NPC npc = new NPC(npcName, npcObject, 100, 100, 5, 0, dialogues);
+      NPC npc = new NPC(npcName, npcObject, 100, 100, 5, 0, DialogueList.Dialogues[npcName]);
       ConnectedCharacters.NPCs.Add(npcId, npc);
       ConnectedCharacters.NPCs[npcId].Avatar.GetComponent<CharacterLerpController>().Character = npc;
     }
