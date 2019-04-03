a23004a60b62c0746027a835e66254ffcfa1b9c2
Jeffrey Thor <thorjeffrey@gmail.com>
2019-04-02 22:04:17 -0400

    Adds NPC Creator editor functionality

--- Assets/Networking/Constants/EndpointConstants.cs
+++ Assets/Networking/Constants/EndpointConstants.cs
@@ -2,5 +2,6 @@ namespace Networking.Constants {
   public static class EndpointConstants {
     public const string Login = "login.php";
     public const string Register = "register.php";
+    public const string CreateNPC = "create_npc.php";
   }
 }
