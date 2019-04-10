becc51222ec85247dcd0f3949427b9977b3cd6d4
Jeffrey Thor <thorjeffrey@gmail.com>
2019-04-10 13:54:16 -0400

    Adds NPC destination creator prefab

--- Assets/Networking/Constants/EndpointConstants.cs
+++ Assets/Networking/Constants/EndpointConstants.cs
@@ -3,6 +3,7 @@ namespace Networking.Constants {
     public const string Login = "login.php";
     public const string Register = "register.php";
     public const string CreateNPC = "create_npc.php";
+    public const string CreateNPCDestination = "add_destination_for_npc.php";
     public const string CreateItem = "add_world_item.php";
   }
 }
